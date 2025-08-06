396649776002df5e304b1c789f9f38fba2188c77
Elias Jacome <eliasjacome77@gmail.com>
2025-08-05 22:12:59 -0500

    Back9

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ app.use(express.json());
 
 // Pool de conexiones MySQL
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  connectionLimit: 10
 });
+
 
 
 db.connect(err => {
